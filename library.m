2c87e11911c038e625f38ee064d399ff3e4309ce
alaface <alaface@users.noreply.github.com>
2020-10-14 14:47:47 +0200

    Add files via upload

--- library.m
+++ library.m
@@ -109,3 +109,70 @@ TestDef := function(pts,k)
   return true,lis[i];
  end if;
 end function;
+
+
+
+// CoplanarPoints
+// INPUT: a polytope pol
+// OUTPUT: all the subsets of > dim(pol) points 
+// which lie on a hyperplane
+
+CoplanarPoints := function(pol)
+ d := Dimension(pol);
+ pts := Points(pol);
+ cl := {};
+ for S in Subsets(pts,d) do
+  p := Random(S);
+  L := [q-p : q in S];
+  N := {q : q in pts | Rank(Matrix(L)) eq Rank(Matrix(L cat [q-p]))};
+  if #N gt d then Include(~cl,N); end if;
+ end for;
+ return [C : C in cl | not &or{C subset Q : Q in cl diff {C}}];
+end function;
+
+
+// IsSpecial
+// INPUT: a polytope pol
+// OUTPUT: true if the toric variety defined by pol
+// admits a defective k-secant variety for some k
+// false otherwise
+
+IsSpecial := function(pol)
+ N := Ambient(pol);
+ P := pol-N![Min(Eltseq(R)) : R in Rows(Transpose(Matrix(Vertices(pol))))];
+ d := Dimension(P);
+ Q := GF(32003);
+ A<[x]> := AffineSpace(Q,d);
+ R := CoordinateRing(A);
+ mon := [Monomial(R,Eltseq(p)) : p in Points(P)];
+ L := LinearSystem(A,mon);
+ r := Floor(#mon/(d+1));
+ pts := [A![Random(Q) : i in [1..d]] : j in [1..r+1]];
+ ls1 := LinearSystem(L,pts[1..r],[2 : i in [1..r]]);
+ ls2 := LinearSystem(ls1,[pts[r+1]],[2]);
+ if #mon - r*(d+1) lt Dimension(ls1) 
+  then return true;
+ elif Dimension(ls2) gt 0
+  then return true;
+ end if;
+ return false;
+end function;
+
+
+// ProjSpace
+// INPUT: positive integers m,d
+// OUTPUT: the polytope of P^m embedded with degree d
+
+ProjSpace := function(m,d)
+ return Polytope([[0: i in [1..m]]] cat [[0: i in [1..j-1]] cat [d] cat [0: i in [j+1..m]]:  j in [1..m]]);
+end function;
+
+
+// ProjSpaces
+// INPUT: two lists of positive integers n,d
+// OUTPUT: the polytope of P^n_1 x ... x P^n_k 
+// embedded with multidegree [d_1,...,d_k]
+
+ProjSpaces := function(n,d)
+ return &*[ProjSpace(n[i],d[i]) : i in [1..#n]];
+end function;
